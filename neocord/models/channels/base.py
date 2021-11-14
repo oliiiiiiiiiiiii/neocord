08502a0042d80eb5d899ba422342cdceaf3a5d7d
NerdGuyAhmad <thisisizharahmad@gmail.com>
2021-11-14 18:15:22 +0500

    Add basic guild channel model.

---
+++ neocord/models/channels/base.py
@@ -0,0 +1,62 @@
+# MIT License
+
+# Copyright (c) 2021 Izhar Ahmad
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from __future__ import annotations
+from typing import TYPE_CHECKING, Any
+
+from neocord.internal import helpers
+
+if TYPE_CHECKING:
+    from neocord.models.guild import Guild
+
+class GuildChannel:
+    """
+    Base class that implements basic operations for all channels types in a guild.
+
+    Currently, the classes that inherit this are:
+
+    - :class:`TextChannel`
+    - :class:`VoiceChannel`
+    - :class:`StageChannel`
+    - :class:`CategoryChannel`
+    - :class:`StoreChannel`
+
+    Private channels like :class:`DMChannel` do not inherit this class.
+    """
+    # TODO: Add GuildChannelPayload
+    def __init__(self, data: Any, guild: Guild):
+        self.guild = guild
+        self._state = self.guild._state
+        self._update(data)
+
+    def _update(self, data: Any):
+        self.id = helpers.get_snowflake(data, 'id')
+        self.guild_id = helpers.get_snowflake(data, 'guild_id')
+        self.parent_id = helpers.get_snowflake(data, 'parent_id')
+
+        self.type = int(data['type'])
+        self.position = int(data.get('position', 0))
+        self.name = data.get('name')
+
+        # TODO
+        self._permissions_overwrite = data.get('permissions_overwrite')
+        self._permissions = data.get('permissions')
