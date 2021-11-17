9214a8ffb39f73a7919a9a3e8c72a1892e482993
NerdGuyAhmad <thisisizharahmad@gmail.com>
2021-11-18 02:38:57 +0500

    Only update the fields that are subject to be changed.

--- neocord/models/message.py
+++ neocord/models/message.py
@@ -21,13 +21,17 @@
 # SOFTWARE.
 
 from __future__ import annotations
-from typing import TYPE_CHECKING
+from neocord.api.routes import guild
+from typing import TYPE_CHECKING, Optional
 
 from neocord.models.base import DiscordModel
+from neocord.models.member import GuildMember
+from neocord.models.user import User
 from neocord.internal import helpers
 
 if TYPE_CHECKING:
     from neocord.api.state import State
+    from neocord.models.guild import Guild
     from neocord.typings.message import Message as MessagePayload
 
 class Message(DiscordModel):
@@ -40,23 +44,47 @@ class Message(DiscordModel):
     """
     __slots__ = (
         'id', 'channel_id', 'guild_id', 'content', 'timestamp', '_edited_timestamp',
-        'tts', 'mention_everyone', 'pinned', 'type',
+        'tts', 'mention_everyone', 'pinned', 'type', 'webhook_id', 'author', 'state'
     )
 
     def __init__(self, data: MessagePayload, state: State) -> None:
+        self.state = state
+        self.channel_id = helpers.get_snowflake(data, 'channel_id')
+        self.webhook_id = helpers.get_snowflake(data, 'webhook_id')
+        self.id = int(data['id'])
+        self.guild_id = helpers.get_snowflake(data, 'guild_id')
+        self.timestamp = helpers.iso_to_datetime(data.get('timestamp'))
+        self.tts = data.get('tts', False)
+        self.type = data.get('type')
+        self.author = None # type: ignore
+
+        author = data.get('author')
+
+        if self.webhook_id is None:
+            if self.guild:
+                # since the member is most likely to be partial here, we try to
+                # obtain member from our cache and in case we fail, we will
+                # resolve it to user.
+                self.author = self.guild.get_member(int(author['id']))
+
+            if self.author is None:
+                self.author = User(author, state=self.state)
+
         self._update(data)
 
     def _update(self, data: MessagePayload):
-        self.id = int(data['id'])
-        self.channel_id = helpers.get_snowflake(data, 'channel_id')
-        self.guild_id = helpers.get_snowflake(data, 'guild_id')
+        # this only has the fields that are subject to change after
+        # initial create.
         self.content = data.get('content')
 
-        self.timestamp = helpers.iso_to_datetime(data.get('timestamp'))
         self._edited_timestamp = data.get('edited_timestamp')
-
-        self.tts = data.get('tts', False)
-        self.mention_everyone = data.get('mention_everyone', False)
         self.pinned = data.get('pinned', False)
+        self.mention_everyone = data.get('mention_everyone', False)
 
-        self.type = data.get('type')
+    @property
+    def guild(self) -> Optional[Guild]:
+        """
+        :class:`Guild`: Returns the guild in which message was sent. Could be None
+        if message was sent in a DM channel.
+        """
+        return self.state.get_guild(self.guild_id) # type: ignore
